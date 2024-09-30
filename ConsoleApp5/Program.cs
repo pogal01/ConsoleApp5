4fbff56589140d573d5e5a60e7268fb5f854845f
Purna <purna@users.noreply.github.com>
2024-09-30 13:39:56 +0100

    added comment

--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -12,7 +12,7 @@
             Console.WriteLine("input second number");
             input2 = Convert.ToInt32(Console.ReadLine());
 
-
+            // Chreck for inputs being equal
             if (input1 == input2)
             {
                 Console.WriteLine(input1 + " and "+ input2 +" are equal");
@@ -20,6 +20,8 @@
             {
                 Console.WriteLine(input1 + " and " + input2 + " are not equal");
             }
+
+
         }
     }
 }
