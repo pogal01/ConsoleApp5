effc22390104ec4abeb490d6e2f1e513acbfba2d
Purna <purna@users.noreply.github.com>
2024-09-30 13:35:19 +0100

    Initial commit

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int input1, input2;
+            
+            Console.WriteLine("input number");
+            input1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("input second number");
+            input2 = Convert.ToInt32(Console.ReadLine());
+
+
+            if (input1 == input2)
+            {
+                Console.WriteLine(input1 + " and "+ input2 +" are equal");
+            } else
+            {
+                Console.WriteLine(input1 + " and " + input2 + " are not equal");
+            }
+        }
+    }
+}
